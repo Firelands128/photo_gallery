3db02aa2ec3b397cc98c448214b7f5e1e01823db
Wenqi Li <firelands128@gmail.com>
2023-05-05 23:38:02 +0800

    remove unused imports

--- lib/photo_gallery.dart
+++ lib/photo_gallery.dart
@@ -2,11 +2,9 @@ library photogallery;
 
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
